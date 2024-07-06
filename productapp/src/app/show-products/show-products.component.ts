491809fd3dbf9fae3257b38fb6ee6d363b599cfa
Manikanta Jalla <manikanta.jalla8@gmail.com>
2024-07-06 17:15:02 -0400

    added files

---
+++ productapp/src/app/show-products/show-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-products.component.html',
+  styleUrl: './show-products.component.css'
+})
+export class ShowProductsComponent {
+
+}
